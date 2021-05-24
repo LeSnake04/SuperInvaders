ef47553a70ce1bc6aa2b81edd5dedcab46d001e8
LeSnake04 <dev.lesnake@posteo.de>
2021-05-24 23:21:08 +0200

    Cleanup

--- superinvaders.js
+++ superinvaders.js
@@ -1,10 +1,9 @@
-let CanvasSetup = {
+const CanvasSetup = {
 	X : 400,
 	Y : 600
 };
 
-let PlayerSetup = {
-//FIXME: Player Not showing
+const PlayerSetup = {
 X : ((CanvasSetup.X/2) - (CanvasSetup.X*0.05)),
 Y : (CanvasSetup.Y*0.95),
 Size : (CanvasSetup.X*0.1),
@@ -14,21 +13,20 @@ Color : [255,255,0]
 function setup() {
 	createCanvas(CanvasSetup.X,CanvasSetup.Y);
 	background(40);
-	console.log(PlayerSetup);
-	Player=circle(PlayerSetup.X,PlayerSetup.Y,PlayerSetup.Size);
-	Player.x=PlayerSetup.XM
+	fill(PlayerSetup.Color);
+	Player=circle(PlayerSetup.X,PlayerSetup.Y,PlayerSetup.Size)
 }
 
 function draw() {
-	Player.fill(PlayerSetup.Color);
+
 }
 
 function keyTyped(){
 	draw()
 	if (key === 'a') {
-		Player.x(-5);
+		x += -5;
 	} else if (key === 'd'){
-		Player.x(5);
+		x += 5;
 	} else {
 		console.log("Key not bound:",key);
 	}
